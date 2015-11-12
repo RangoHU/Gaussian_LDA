fa7557b02adfe505dc304ba9985ac77426b39c39
Tianran Hu <thu@f07.cs.rochester.edu>
2015-11-12 08:12:46 -0500

    minor modifications

--- G_LDA_1.py
+++ G_LDA_1.py
@@ -3,7 +3,7 @@ import numpy
 import re
 from optparse import OptionParser
 
-def main():
+def test():
 	#parser = OptionParse()
 	#parse.add_option()
 	T = 
@@ -25,12 +25,11 @@ def main():
 	for i in range(iteration):
 		glad.inference()
 	
-	phi = glad.topic_doc()
+	phi = glad.doc_topic()
 	g_mu, g_sigma = glad.gaussian_topic()
 
 
-if __name__ == "__main__":
-	main()
+
 
 class GLDA:
 	def __init__(self, T, alpha, K, NU, MU, SIGMA, D):
@@ -64,8 +63,10 @@ class GLDA:
 		line = input.readline()
 		while line != '':
 			temp = line.split(':')
+			word = temp[0]
+			lst = temp[1].split(',')
 			#convert string array to float array, store them in map
-			self.wordvec[temp[0]] = map(float, temp[1])
+			self.wordvec[word] = numpy.array(map(float, lst))
 			line = input.readline()
 		input.close()
 
@@ -73,26 +74,31 @@ class GLDA:
 
 
 	def term_to_id(self, term):
+		if term not in self.wordvec:
+			return None
 		if term not in self.vocas_id:
-			voca_id = len(self.vocas)
+			voca_id = len(self.vocas_id)
 			self.vocas_id[term] = voca_id
 			self.re_vocas_id[voca_id] = term
-			self.vocas.append(term)# don't see the function of this sentence here
+			##self.vocas.append(term)# don't see the function of this sentence here
 		else:
 			voca_id = self.vocas_id[term]
 		return voca_id
 	
 	def set_corpus(self): #more like initialize
-		self.vocas = []
+		##self.vocas = []
 		self.vocas_id = {}
 		self.re_vocas_id = {}
 		#self.docs is a 2-D array;
 		#each row is a document;
 		#a document is represented by a vecotor;
 		#element in this vector is word id.
-		self.docs = [[self.term_to_id(term) in doc] for doc in corpus]
+		#also get rid of the word not included in wordvec 
+		temp = [[self.term_to_id(term) in doc] for doc in corpus]
+		self.docs = [[word_id in doc if word_id is not None] for doc in temp]
 		M = len(corpus)#number of documents
-		V = len(self.vocas)#number of terms, not words
+		##V = len(self.vocas)#number of terms, not words
+		V = len(self.vocas_id)#number of terms, not words
 		self.z_m_n = []#topic assignment of each word in docs
 		#in a document, how many words are assigned to a topic
 		self.n_m_z = numpy.zeros((M, self.T), dtype = int) #document topic dist.
@@ -126,12 +132,12 @@ class GLDA:
 			#compute the average of each topic
 			for v in range(len(self.vocas)):
 				times = self.n_z_t[t][v]
-				average[t] += wordvec[self.re_vocas_id[v]] * times #this word appears 'times' on this topic
+				average[t] += self.wordvec[self.re_vocas_id[v]] * times #this word appears 'times' on this topic
 			average[t] = average[t] / self.n_z[t]
 			#compute the variance of each topic			
 			for v in range(len(self.vocas)):
 				times = self.n_z_t[t][v]
-				variance[t] += (wordvec[self.re_vocas_id[v]] - average[t]) ** 2 * times
+				variance[t] += (self.wordvec[self.re_vocas_id[v]] - average[t]) ** 2 * times
 		#step2 : compute mu and sigma
 		#compute mu
 		#self.K is a D-dimensional vector
@@ -155,6 +161,7 @@ class GLDA:
 			shape_p = self.NU + self.n_z[t]
 			loc_p = mu[t]
 			scale_p = sigma[t] ** 0.5
+			#for each topic, there are D likelihoods
 			for d in range(self.D):
 				d_likelihood.append(scipy.stats.t.pdf(wordvec[self.re_vocas_id[word_id]][d], df = shape_p[d], loc = loc_p[d], scale = scale_p[d]))
 			#step4 : compute the multiplication of all dimensions
@@ -165,8 +172,9 @@ class GLDA:
 
 
 	def inference(self):
-		V = len(self.vocas)
-		#m is the m_th document, doc is the m_th document's words
+		##V = len(self.vocas)
+		V = len(self.vocas_id)
+		#m is id of document, doc is the m_th document's words
 		for m, doc in zip(range(len(self.docs)), self.docs):
 			#iterate all the word in this document
 			for n in range(len(doc)):
@@ -176,21 +184,21 @@ class GLDA:
 				self.n_m_z[m, z] -= 1
 				self.n_z_t[z, t] -= 1
 				self.n_z[z] -= 1
-			#doesn't really need a denom
-			#gassian_likelihood is the only difference from original
-			#we are considering make it two gassian later.
-			p_z = gassian_likelihood(t) * (self.n_m_z[m] + self.alpha)
-			new_z = numpy.random.multinomial(1, p_z / p_z.sum()).argmax()
-			self.z_m_n[m][n] = new_z
-			self.n_m_z[m, new_z] += 1
-			self.n_z_t[new_z, t] += 1
-			self.n_z[new_z] += 1
+				#doesn't really need a denom
+				#gassian_likelihood is the only difference from original
+				#we are considering make it two gassian later.
+				p_z = gassian_likelihood(t) * (self.n_m_z[m] + self.alpha)
+				new_z = numpy.random.multinomial(1, p_z / p_z.sum()).argmax()
+				self.z_m_n[m][n] = new_z
+				self.n_m_z[m, new_z] += 1
+				self.n_z_t[new_z, t] += 1
+				self.n_z[new_z] += 1
 
 	
 	#topic distribution in each doc
 	#return a M by T matrix
 	#each row is the distribution each T topic is m_th document.
-	def topic_doc(self):
+	def doc_topic(self):
 		return (self.n_m_z + self.alpha) / (self.n_m_z.sum(axis = 1)[:, numpy.newaxis] + self.alpha)
 
 
@@ -203,11 +211,11 @@ class GLDA:
 		for t in range(self.T):
 			for v in range(len(self.vocas)):
 				times = self.n_z_t[t][v]
-				average[t] += wordvec[self.re_vocas_id[v]] * times #this word appears 'times' on this topic
+				average[t] += self.wordvec[self.re_vocas_id[v]] * times #this word appears 'times' on this topic
 			average[t] = average[t] / self.n_z[t]
 			for v in range(len(self.vocas)):
 				times = self.n_z_t[t][v]
-				variance[t] += (wordvec[self.re_vocas_id[v]] - average[t]) ** 2 * times
+				variance[t] += (self.wordvec[self.re_vocas_id[v]] - average[t]) ** 2 * times
 		mu = numpy.zeros((self.T, self.D), dtype = float)
 		for t in range(self.T):
 			mu[t] = (self.K * self.MU +  self.n_z[t] * average[t]) / (self.K + self.n_z[t])
