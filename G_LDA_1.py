7f2da2284e8176303fbd43de3e25e4a95c987760
Tianran Hu <thu@f07.cs.rochester.edu>
2015-12-05 16:44:25 -0500

    fix bug

--- G_LDA_1.py
+++ G_LDA_1.py
@@ -8,17 +8,17 @@ from scipy.special import gammaln as gamln
 from scipy.spatial.distance import cosine
 from scipy.spatial.distance import *
 import datetime
+
+
 '''
-
-
 from G_LDA_1 import *
 T = 10
 D = 300
-alpha = numpy.array([1.0] * T) / T
+alpha = 0.001 #numpy.array([0.001] * T) 
 K = 1.0
-MU = numpy.array([0.0] * D)
+MU = 0.0 #numpy.array([0.0] * D)
 NU = 1.0
-SIGMA = numpy.array([1.0] * D)
+SIGMA = 0.1 #numpy.array([0.1] * D)
 
 wordvec_file = 'toy_word2vec'
 corpus_file = 'summary_doc'
@@ -28,10 +28,12 @@ glad.load_corpus(corpus_file)
 glad.set_corpus()
 
 
-for i in range(10):
+
+for i in range(20):
 	print datetime.datetime.now()
 	print i
 	glad.inference()
+
 
 print glad.n_m_z
 
@@ -117,9 +119,6 @@ class GLDA:
                                 self.n_m_z[m, z] += 1
                                 self.n_z_t[z, t] += 1
                                 self.n_z[z] += 1
-		'''
-		calculate mu, sigma, kappa, nu for initialized tpics.
-		'''
 		self.mu = []
 		self.sigma = []
 		self.nu = []
@@ -127,8 +126,9 @@ class GLDA:
 		self.summary = []
 		self.variance = []
 		self.average = []
+		self.first_part = []
 		for i in range(self.T):
-			mu, sigma, kappa, nu, summary, variance, average = self.update_topic(i)
+			mu, sigma, kappa, nu, summary, variance, average, first_part = self.init_topic(i)
 			self.mu.append(mu)
 			self.sigma.append(sigma)
 			self.kappa.append(kappa)
@@ -136,6 +136,7 @@ class GLDA:
 			self.summary.append(summary)
 			self.variance.append(variance)
 			self.average.append(average)
+			self.first_part.append(first_part)
 		self.mu = numpy.array(self.mu)
 		self.sigma = numpy.array(self.sigma)
 		self.nu = numpy.array(self.nu)
@@ -143,107 +144,78 @@ class GLDA:
 		self.summary = numpy.array(self.summary)
 		self.variance = numpy.array(self.variance)
 		self.average = numpy.array(self.average)
+		self.first_part = numpy.array(self.first_part)
 
 
-	def update_topic(self, z):
-
+	def init_topic(self, z):
 		times = self.n_z_t[z][self.n_z_t[z] > 0]
 		word_vec = self.wordvec[self.n_z_t[z] > 0]
 		summary = numpy.dot(times, word_vec)
-		#total_time = times.sum()
 		total_time = self.n_z[z]
 		average = summary / total_time
 		variance = numpy.dot(times, (word_vec - average) ** 2)
 		kappa = self.Kappa + total_time
 		nu = self.Nu + total_time
+
 		mu = (self.Kappa * self.Mu + summary) / kappa
 
-		sigma = (1 + kappa) / (nu * kappa) * \
-			(self.Nu * (self.Sigma ** 2) + variance + \
-			total_time * self.Kappa / (self.Kappa + total_time) * (self.Mu - average)) ** 2 
+		sigma = (1 + kappa) / (kappa) * \
+			(self.Nu * (self.Sigma) + variance + \
+			total_time * self.Kappa / (kappa) * (self.Mu - average)**2 )
 
-		return mu, sigma, kappa, nu, summary, variance, average
+		first_part = self.D * (gamln((nu + 1) / 2) - gamln(nu / 2) - numpy.log(nu) / 2)
+
+		return mu, sigma, kappa, nu, summary, variance, average, first_part
 
 
 	'''
 	TODO: self.Mu is usually set to be zero!
 	'''
+
 	def cal_mu(self, kappa, summary):
 		return (self.Kappa * self.Mu + summary) / kappa
 
 
-	def cal_sigma(self, kappa, nu, variance, total_time, average):
-		return 	(1 + kappa) / (nu * kappa) * \
-			(self.Nu * (self.Sigma ** 2) + variance + \
-			total_time * self.Kappa / (self.Kappa + total_time) * (self.Mu - average)) ** 2 
+	def cal_sigma(self, kappa, variance, total_time, average):
+		return 	(1 + kappa) / (kappa) * \
+			(self.Nu * (self.Sigma) + variance + \
+			total_time * self.Kappa / (kappa) * (self.Mu - average)**2 ) 
 
 
-	def subtract_update_topic(self, z, t, flag):
+	def update_topic(self, z, t, flag):
 		vec = self.wordvec[t]
-
 		total_time = self.n_z[z]
-
 		if flag == '-':
-			#new_summary = summary - vec
 			self.summary[z] -= vec
 			temp_average = self.average[z]
 			self.average[z] = self.summary[z] / total_time
 			self.variance[z] -= (vec - temp_average) * (vec - self.average[z])
 			self.nu[z] -= 1
 			self.kappa[z] -= 1
+			self.first_part[z] = self.D * (gamln((self.nu[z] + 1) / 2) - gamln(self.nu[z] / 2) - numpy.log(self.nu[z]) / 2)
 		else:
-                        #new_summary = summary - vec
                         self.summary[z] += vec
 			temp_average = self.average[z]
                         self.average[z] = self.summary[z] / total_time
                         self.variance[z] += (vec - temp_average) * (vec - self.average[z])
                         self.nu[z] += 1
                         self.kappa[z] += 1
-
-
+			self.first_part[z] = self.D * (gamln((self.nu[z] + 1) / 2) - gamln(self.nu[z] / 2) - numpy.log(self.nu[z]) / 2)
 		self.mu[z] = self.cal_mu(self.kappa[z], self.summary[z])
-		self.sigma[z] = self.cal_sigma(self.kappa[z], self.nu[z], self.variance[z], total_time, self.average[z])
-		
+		self.sigma[z] = self.cal_sigma(self.kappa[z], self.variance[z], total_time, self.average[z])		
 
 
-	#give topic i, word x, calculate the 
-	def student_t(self, x, i):
-		mu = self.mu[i]
-		sigma = self.sigma[i]
-		kappa = self.kappa[i]
-		nu = self.nu[i]
-		first_part = self.D * (gamln((nu + 1) / 2) - gamln(nu / 2) - numpy.log(nu * pi) / 2)
-		#temp = numpy.log(1 + ((x - mu) ** 2) / (sigma * nu)) * 2 #incorrect
-		temp = numpy.log(1 + ((x - mu) ** 2) / (sigma * nu)) * (1 - nu) / 2
-		second_part = temp.sum()
-		return first_part + second_part
-
-
-	'''
-	TODO: instead of a loop, can we calculate the whole matrix once?
-	'''
+	@profile
 	def gassian_likelihood(self, x):
-		results = []
-		for i in range(self.T):
-			results.append(self.student_t(x, i))
-		temp = numpy.array(results)
-		temp = temp - temp.max()
-		likelihood = numpy.exp(temp)
-		return likelihood
-
-
-
-	def new_gassian_likelihood(self, x):
-
-		first_part = self.D * (gamln((self.nu + 1) / 2) - gamln(self.nu / 2) - numpy.log(self.nu) / 2)
-		temp = numpy.log(1 + ((x - self.mu) ** 2) / (self.sigma * self.nu[:, numpy.newaxis])) * (1 - self.nu[:, numpy.newaxis]) / 2
+		#first_part = self.D * (gamln((self.nu + 1) / 2) - gamln(self.nu / 2) - numpy.log(self.nu) / 2)
+		temp = (1 - self.nu[:, numpy.newaxis]) / 2  *  numpy.log(1 + ((x - self.mu) ** 2) / (self.sigma)) 
 		second_part = temp.sum(axis = 1)
-		temp = first_part + second_part
+		temp = self.first_part + second_part
 		temp -= temp.max()		
-		#likelihood = numpy.exp(temp)
 		return numpy.exp(temp)
 
 
+	@profile
 	def inference(self):
 		V = len(self.word_id)
 		for m, doc in zip(range(len(self.docs)), self.docs):
@@ -255,34 +227,21 @@ class GLDA:
 				self.n_z_t[z, t] -= 1
 				self.n_z[z] -= 1
 
-				'''
-				TODO: what's the chance of being assigned to the same topic?
-				'''
-				#print z
-				#print datetime.datetime.now()
-				self.subtract_update_topic(z, t, '-')
+				self.update_topic(z, t, '-')
 
-				#print datetime.datetime.now()
-
-				#word_topic = self.gassian_likelihood(t)
-				word_topic = self.new_gassian_likelihood(t)
+				word_topic = self.gassian_likelihood(t)
 				topic_document =  self.n_m_z[m] + self.alpha
 				p_z = word_topic * topic_document
 				new_z = numpy.random.multinomial(1, p_z / p_z.sum()).argmax()
-
-
-				#print datetime.datetime.now()
-				self.subtract_update_topic(new_z, t, '+')
-
-				#print datetime.datetime.now()
-				#print new_z
 				self.z_m_n[m][n] = new_z
                                 self.n_m_z[m, new_z] += 1
                                 self.n_z_t[new_z, t] += 1
                                 self.n_z[new_z] += 1
 
 
-				#return 
+				self.update_topic(new_z, t, '+')
+
+
 
         def phi(self):
                 #V = len(self.word_id)
@@ -311,6 +270,26 @@ class GLDA:
 
 
 
+
+T = 10
+D = 300
+alpha = numpy.array([1.0] * T) / T
+K = 1.0
+MU = numpy.array([0.0] * D)
+NU = 1.0
+SIGMA = numpy.array([1.0] * D)
+
+wordvec_file = 'toy_word2vec'
+corpus_file = 'summary_doc'
+glad = GLDA(T, alpha, K, NU, MU, SIGMA, D)
+glad.load_wordvec(wordvec_file)
+glad.load_corpus(corpus_file)
+glad.set_corpus()
+
+print 'Start running'
+print datetime.datetime.now()
+glad.inference()
+glad.inference()
 
 
 
